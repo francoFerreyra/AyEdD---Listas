2de62f0578b3dc99a3384f45abab0f3002ecd766
francoFerreyra <fran.1993>
2016-04-15 09:30:22 -0300

    guia de listas

--- Listas.cpp
+++ Listas.cpp
@@ -1,4 +1,6 @@
 //
 // Created by usuario on 15/04/2016.
 //
+#include <iostream>
+#include "Listas.hista.h"
 
--- Listas.h
+++ Listas.h
@@ -5,4 +5,35 @@
 #ifndef AYEDD_LISTAS_LISTAS_H
 #define AYEDD_LISTAS_LISTAS_H
 
+#include <iostream>
+#include "nodo.h"
+
+class Lista
+{
+private:
+    //necesitamos un tamaño para la lista y un puntero iniciador
+    int tamanio;
+    nodo*primero;
+public:
+    //necesito un constructor, destructor
+    //una funcion que adelante y retroceda por los nodos
+
+    lista();
+    lista(char p);
+    ~lista();
+    bool esVacio();
+
+    int eliminar(int pos);
+    void agregarincio(char p);
+    void agregarfin(char p);
+    void agregarentre(int pos, char p);
+    int CargarTamanio();
+    int buscar(char p);
+    void imprimir();
+    char CargarDato(int pos, int *err);
+    char CargarDato(int pos);
+    char VerDato(int pos, char p);
+
+
+};
 #endif //AYEDD_LISTAS_LISTAS_H
