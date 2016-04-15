b098404967ea23de9fb5adc3743af66985b0b3c1
francoFerreyra <fran.1993>
2016-04-15 13:00:52 -0300

    guia de listas

--- Nodo.cpp
+++ Nodo.cpp
@@ -1,4 +1,43 @@
 //
 // Created by usuario on 15/04/2016.
 //
+#include <iostream>
+#include "Nodo.h"
 
+nodo::nodo()
+{
+    dato = 0;
+    next = NULL;
+}
+nodo::nodo(char d)
+{
+    dato = d;
+    next=NULL;
+}
+char nodo::CargarDato()
+{
+    return dato;
+}
+void nodo::VerDato(char d)
+{
+    dato= d;
+}
+nodo* nodo::CargarProximo()
+{
+    return next;
+}
+void nodo::VerProximo(nodo *d)
+{
+    next = d;
+}
+nodo* nodo::NuevoNext()
+{
+    next = new nodo();
+    return next;
+}
+nodo* nodo::CargarProximo()
+{
+    tamanio= 1;
+    next= NuevoNext();
+    
+}
